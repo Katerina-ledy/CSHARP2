693a4a133b8aa7207ce5db05f744244ab71d7f1d
Alekper <alekper@ya.ru>
2024-10-31 21:35:42 +0300

    add homework lesson1:task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -23,3 +23,16 @@ Console.WriteLine(quotient);
 d1 = 6;
 double quotient2 = i1 / d1;
 Console.WriteLine(quotient2);
+
+// Homework
+
+int a = 3;
+int b = 4;
+int c = 2;
+int d = 3;
+int product4 = a * b;
+double sum5 = c + d;
+double quotient3 = product4 / sum5;
+ Console.WriteLine(product4);
+ Console.WriteLine(sum5);
+ Console.WriteLine(quotient3);
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0894d3c782e493fe7ef90f34588fbea41e7125bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860a2b693259185d9e4e604fb6d870c5ef313748")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
